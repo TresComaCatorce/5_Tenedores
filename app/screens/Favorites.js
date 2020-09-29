c78b373b5af3ab9b46348ff278777dae5384bd6d
Cristian Ferrero <cristian.ferrero@dirmod.com>
2020-09-29 19:43:32 -0300

    Navigation system

---
+++ app/screens/Favorites.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default Favorites = props => {
+	return <View>
+		<Text>
+		Favorites
+		</Text>
+	</View>
+};
---
+++ app/screens/TopRestaurants.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default TopRestaurants = props => {
+	return <View>
+		<Text>
+			TopRestaurants
+		</Text>
+	</View>
+};
